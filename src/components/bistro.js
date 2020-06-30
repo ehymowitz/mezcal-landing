ed623f7c2df26cfce58339cfad25b1b4d8b0d243
Elie Hymowitz <ehymowitz@gmail.com>
2020-06-29 21:38:20 -0400

    components files made

---
+++ src/components/bistro.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+
+
+export default function Bistro() {
+  return(
+    <p>bistro</p>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from "react"
+import Bistro from '../components/bistro'
+import Cafe from '../components/cafe'
+import Form from '../components/form'
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="Container">
+      <div className="Top">
+        <Bistro/>
+        <Cafe/>
+      </div>
+      <div className="Form">
+        <Form/>
+      </div>
+    </div>
+
+  )
 }
