5b19c50ee3ac09ba7666be04300b215e4ff59261
Elie Hymowitz <ehymowitz@gmail.com>
2020-07-24 15:53:42 -0400

    added meta description

--- src/components/container.js
+++ src/components/container.js
@@ -5,8 +5,8 @@ export default function Container({ children }) {
   return (
     <div className="container">
       <Helmet defer={false} >
-        <title>Mezcal</title>
-        <meta name="Mezcal Montreal" content= "Fresh Fish Tacos, Guacamole, Ceviche and Shrimp Cocktail. Also your regular homemade Mexican soup, burritos, nachos. Amazing Drinks!"/>
+        <title>Mezcal Montreal</title>
+        <meta name="description" content= "Fresh Fish Tacos, Guacamole, Ceviche and Shrimp Cocktail. Also your regular homemade Mexican soup, burritos, nachos. Amazing Drinks!"/>
         <meta property="og:description" content="Fresh Fish Tacos, Guacamole, Ceviche and Shrimp Cocktail. Also your regular homemade Mexican soup, burritos, nachos. Amazing Drinks!" />
         <meta property="og:image" content='/preview.png' />
         <meta property="og:url" content='https://mezcalmtl.ca/'/>
